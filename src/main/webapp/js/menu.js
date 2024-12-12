39805010c1759bbd8f1e7ea6fe92f21ca1cd3d27
Henrico <128430182+HenricoJustino@users.noreply.github.com>
2024-12-12 15:26:10 -0300

    Update menu.js

--- src/main/webapp/js/menu.js
+++ src/main/webapp/js/menu.js
@@ -1,3 +1,7 @@
+var menu = ` 
+<head>
+<link rel='stylesheet' type='text/css' media='screen' href='/backup/backup-menu.css'>
+</head>
 <nav id="menu">
         <h1 class="logo-menu"><a class="logo-menu" href="">ConstruMax</a></h1>
         <div class="area-pesquisa">
@@ -11,7 +15,7 @@
         </button>
         <ul class="alinhameto-botao-menu" id="menu-mobile">
             <li class="botao-menu">
-                <a class="nome">Exemplo1</a>
+                <a class="nome">Categorias</a>
                 <div class="drop-bar">
                     <a href="" class="drop">Teste1</a>
                     <a href="" class="drop">Teste2</a>
@@ -20,7 +24,7 @@
                 </div>
             </li>
             <li class="botao-menu">
-                <a class="nome">Exemplo2</a>
+                <a class="nome">Contato</a>
                 <div class="drop-bar">
                     <a href="" class="drop">Teste1</a>
                     <a href="" class="drop">Teste2</a>
@@ -29,25 +33,22 @@
                 </div>
             </li>
             <li class="botao-menu">
-                <a class="nome">Exemplo3</a>
+                <a class="nome">Configurações</a>
                 <div class="drop-bar">
-                    <a href="" class="drop">Teste1</a>
-                    <a href="" class="drop">Teste2</a>
-                    <a href="" class="drop">Teste3</a>
+                    <a href="/LojaConstrucao/cliente?acao=listarTodos" class="drop">Listar clientes</a>
+                    <a href="/LojaConstrucao/produto?acao=listarProdutos" class="drop">Listar Produtos</a>
+                    <a href="/LojaConstrucao/funcionarios?acao=listarFuncionarios" class="drop">Listar Funcionários</a>
                     <a href="" class="drop">Teste4</a>
                 </div>
             </li>
             <li class="botao-menu">
-                <a class="nome">Exemplo3</a>
-                <div class="drop-bar">
-                    <a href="" class="drop">Teste</a>
-                    <a href="" class="drop">Teste</a>
-                    <a href="" class="drop">Teste</a>
-                    <a href="" class="drop">Teste</a>
-                </div>
+                <a class="nome"><i class="fas fa-shopping-cart"></i> Carrinho</a>
             </li>
-            <li class="botao-menu">
-                <a href="" class="nome nome-login"><i class="fa-solid fa-user"></i>&#160;Login</a>
+            <li class="botao-menu b-login">
+                <a href="/login.html" class="nome-login"><i class="fa-solid fa-user"></i>&#160;Login</a>
             </li>
         </ul>
     </nav>
+`
+document.body.insertAdjacentHTML("afterbegin", menu);
+
